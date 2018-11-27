518bb31684412a4fc496dcd4eef97bd4bac604dd
github username <github email address>
2018-11-27 18:11:36 -0500

    almost.. has one error

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,31 @@
+class StudentsController < ApplicationController
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.create(params[:student])
+    redirect_to student_path(@student)
+  end
+
+ def update
+   @student = Student.find(params[:id])
+   @student.update(params[:student])
+   redirect_to student_path(@student)
+ end 
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name:"ezik", last_name:"gottesfeld")
+Student.create(first_name:"manny", last_name:"shapir")
+Student.create(first_name:"anik", last_name:"islam")
+Student.create(first_name:"noami", last_name:"anom")
+Student.create(first_name:"rquel", last_name:"randel")
+
+
+SchoolClass.create(title: "math", room_number: 1)
+SchoolClass.create(title: "sceince", room_number: 2)
+SchoolClass.create(title: "english", room_number: 3)
